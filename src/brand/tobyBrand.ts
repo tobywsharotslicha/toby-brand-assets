1bcae45d281c0a60e825e149f917f9e916090d8a
tobywsharotslicha <tobyw.sharotslicha@gmail.com>
2026-01-25 09:43:32 +0200

    Create tobyBrand.ts

---
+++ src/brand/tobyBrand.ts
@@ -0,0 +1,14 @@
+// src/brand/tobyBrand.ts
+export const TOBY_BRAND_VERSION = "v1.0.3";
+export const TOBY_BRAND_CDN_BASE = `https://cdn.jsdelivr.net/gh/tobywsharotslicha/toby-brand-assets@${TOBY_BRAND_VERSION}`;
+
+// CSS של הטוקנים/פונטים שלך
+export const TOBY_THEME_CSS_URL = `${TOBY_BRAND_CDN_BASE}/themes/toby.css`;
+
+// ספרייטים (לפי התיקיות שלך)
+export const TOBY_SPRITE_1_URL = `${TOBY_BRAND_CDN_BASE}/brand/icons/sprite/sprite-01.png`;
+export const TOBY_SPRITE_2_URL = `${TOBY_BRAND_CDN_BASE}/brand/icons/sprite/sprite-02.png`;
+export const TOBY_SPRITE_3_URL = `${TOBY_BRAND_CDN_BASE}/brand/icons/sprite/sprite-03.png`;
+
+// רקעים (דוגמה)
+export const TOBY_BG_WINE_01_URL = `${TOBY_BRAND_CDN_BASE}/brand/images/background/red.png`;
